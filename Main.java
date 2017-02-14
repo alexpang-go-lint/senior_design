815829e30459baef66954e42d6300e82c3c8288e
pmerker2014 <pmerker2014@my.fit.edu>
2017-02-14 06:39:29 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,21 +2,44 @@
  *
  */
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
+	
+	static int allowed = 13365;		// 60 percent = 8910, 75% = 11138, 90% = 13365, 100% = 14850
+	static boolean found = false;
+	
+	private static boolean runProcess() throws IOException, FileNotFoundException {
 		ArrayList<ArrayList<String>> arrayLists = new ArrayList<ArrayList<String>>();
 		ProcessResults process = new ProcessResults();
 		process.readFile(arrayLists);
-		CheckSim sim = new CheckSim();
-		arrayLists = sim.disConnectedNodes(arrayLists);
-		arrayLists = sim.process(arrayLists);
+		arrayLists = process.disConnectedNodes(arrayLists,allowed);
+		arrayLists = process.process(arrayLists);
+		if(arrayLists.get(0).size()==4){
+			found = true;
+		}
 		process.getPrediction(arrayLists);
 		process.createSubmission(arrayLists);
+		
+		return found;
+	}
+	
+	
+	public static void main(String[] args) throws IOException {
+		
+		//runProcess();
+		while (!runProcess()){
+			allowed = allowed - 112;
+		}
 		System.out.println("File complete");
+		double num = 0.00;
+		num = allowed;
+		System.out.println("Total Percent Simular : " + num/ 14850);
 		
 	}
+
+	
 
 }
