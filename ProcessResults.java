65383f033662df2358dfbac9ef2856cdcfe0ee1a
pmerker2014 <pmerker2014@my.fit.edu>
2017-02-14 06:40:04 -0500

    Update ProcessResults.java

--- ProcessResults.java
+++ ProcessResults.java
@@ -7,7 +7,60 @@ import java.util.ArrayList;
 
 public class ProcessResults {
 	
+	int allowed;
+	
+	
+	public ArrayList<ArrayList<String>> process(ArrayList<ArrayList<String>> arrayLists){
+		ArrayList<ArrayList<String>> FinishedArray = new ArrayList<ArrayList<String>>();
+		for(int i = 0; i < arrayLists.get(0).size();i++){
+			ArrayList<String> temp = new ArrayList<String>();
+			for (int j = 0; j < arrayLists.size(); j++){
+				temp.add(arrayLists.get(j).get(i));
+			}
+			FinishedArray.add(temp);
+		}
 		
+		return FinishedArray;
+		
+		
+	}
+
+	public ArrayList<ArrayList<String>> disConnectedNodes(ArrayList<ArrayList<String>> arrayLists, int num){
+		allowed = num;
+		Graph graph = new Graph(arrayLists.get(0).size());	// gets the number of columns then does not count the pump number
+		ArrayList<Column> columns = new ArrayList<Column>();	// Makes an array of columns to be filled	
+		ArrayList<ArrayList<String>> FinishedArray = new ArrayList<ArrayList<String>>(); // hold the final array after collumns selected
+		for(int i = 0; i < arrayLists.get(0).size();i++){		// adds columns
+			Column temp = new Column(arrayLists,i);
+			columns.add(temp);
+		}
+		
+		for(int i = 0; i < columns.size();i++){				// cycle through and see which columns are similar
+			
+			for(int j = 0; j < columns.size();j++){
+				
+				if(columns.get(i).simular(columns.get(j),allowed)){
+					graph.addEdge(i, j);
+				}
+			}
+			
+		}
+		System.out.println("___________________________________________________________");
+		System.out.println("                     GRAPH                                 ");
+		graph.print();
+		System.out.println("___________________________________________________________");
+		graph.disconnectedNodes();		// calls object graph to get disconnected nodes
+		
+		System.out.println("***********************************************************");
+		System.out.println("               Algorithms Selected                         ");
+		for(int i = 0; i < graph.disNodes.size();i++){				// cycle through and see which columns are similar
+		FinishedArray.add(columns.get(graph.disNodes.get(i)).column);
+		System.out.println(graph.disNodes.get(i));
+		}
+		System.out.println("***********************************************************");
+		return FinishedArray;
+		
+	}
 		
 	    @SuppressWarnings("resource")
 		public void readFile(ArrayList<ArrayList<String>> arrayLists) throws IOException{ 
